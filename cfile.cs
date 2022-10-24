71f2ea0097f1ddacd6a4c2137e1fc1ff28a08b4e
Usama <usamanadeem405@gmail.com>
2022-10-24 10:32:45 -0700

    COMMIT DONE

---
+++ cfile.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class cfile
+    {
+        int i;
+        public int sum(int a, int b)
+        {
+            i=a+b;
+            returni;
+        }
+    }
+
+    public void Display()
+    {
+        
+    }
+}
